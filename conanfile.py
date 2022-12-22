5d15fdf2b9424d185818d3fce4a7eae87f463389
KatrinaAS <kat@katcodes.dev>
2022-12-22 18:56:45 +0000

    Added conan files

---
+++ conanfile.py
@@ -0,0 +1,46 @@
+from conans import ConanFile, CMake
+from os import environ
+
+
+class SECP256K1Conan(ConanFile):
+    name = "SECP256K1"
+    version = "0.2.0"
+    license = "MIT"
+    author = ""
+    url = "https://github.com/Gigamonkey-BSV/secp256k1"
+    description = "Optimized C library for EC operations on curve secp256k1 "
+    topics = ("Ecliptical Curve", "secp256k1"")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    generators = "cmake"
+    exports_sources = "*"
+    requires = "autoconf/2.71", "automake/1.16.3", "libtool/2.4.6"
+
+    def set_version(self):
+        if "CIRCLE_TAG" in environ:
+            self.version = environ.get("CIRCLE_TAG")[1:]
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def build(self):
+        self.run("./autogen.sh")
+        self.run("./configure --disable-debug --disable-dependency-tracking --disable-silent-rules --prefix=$(pwd)")
+        self.run("make")
+
+    def package(self):
+        self.copy("*.h", dst="include", src="include")
+
+        if self.options.shared == False:
+            self.copy("*.a", dst="lib", keep_path=False)
+            self.copy("*.lib", dst="lib", keep_path=False)
+        else:
+            self.copy("*.lib", dst="lib", keep_path=False)
+            self.copy("*.dll", dst="bin", keep_path=False)
+            self.copy("*.dylib*", dst="lib", keep_path=False)
+            self.copy("*.so", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["secp256k1"]
---
+++ test_package/build/064dcb4d9d20f016bcf77d60df79b0ee6bcef909/CMakeFiles/conan_integration_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/conan_integration_test.dir/conan_integration_test.cpp.o"
+  "CMakeFiles/conan_integration_test.dir/conan_integration_test.cpp.o.d"
+  "bin/conan_integration_test"
+  "bin/conan_integration_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/conan_integration_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
